f6a29f0a362e0ff899b8faed9c83797d85b1c269
Maciej Głowiński <mck00@o2.pl>
2017-09-29 17:43:45 +0200

    refactor

--- src/main/java/mglowinski/library/controller/UserController.java
+++ src/main/java/mglowinski/library/controller/UserController.java
@@ -3,6 +3,7 @@ package mglowinski.library.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,4 +32,5 @@ public class UserController {
 	  public User saveUser(@RequestBody User user) {
 	      return service.create(user);
 	  }
+	  
 }
